736ca696aa6deb1801f44c6cedbb4f3bc5de9a2e
Rickie <rande17@student.sdu.dk>
2018-04-27 13:38:07 +0200

    Changes to the User.java file case is a reserved word so it have been renamed to currentCase, better word might be an idea.

--- Udred/src/Udred/Business/User.java
+++ Udred/src/Udred/Business/User.java
@@ -11,43 +11,69 @@ import java.util.ArrayList;
  *
  * @author Linea Hoffmann
  */
-public class User
-{
-    private String userName;
-    private int workerID;
+public class User {
+
+    private String userName = "Just some init value, not sure wether or not to include it in the constructor";
+    private final int workerID;
     private ArrayList<Integer> caseList;
     private int accessLevel;
 
-    protected User(int workerID)
-    {
+    /**
+     *
+     * @param workerID constructs a user with the ID provided in the parameter
+     * list
+     */
+    protected User(int workerID) {
         this.workerID = workerID;
     }
-    
-    protected int getCase(int caseID)
-    {
-        return 0;
-    }
-    
-//    protected void addCase(Case case)
-//    {
-//
-//    }
-//    protected void removeCase (Case case)
-//    {
-//
-//    }
 
-    protected String getUserName()
-    {
+    /**
+     *
+     * Jeg er ret usikker pp hvad intentionen er her, at caseList er en
+     * ArrayList af ints giver ikke meget mening, med det der ellers er her
+     *
+     * @param caseID takes a caseID that should match a caseID
+     * @return the caseID if it's contained in the caseList otherwise returns -1
+     */
+    protected int getCase(int caseID) {
+        if (caseList.get(caseID) != -1) {
+            return caseList.get(caseID);
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param currentCase add a caseID to the caseList
+     */
+    protected void addCase(Case currentCase) {
+        this.caseList.add(currentCase.getCaseID());
+    }
+
+    /**
+     *
+     * @param currentCase removed a caseID from the caseList
+     */
+    protected void removeCase(Case currentCase) {
+        this.caseList.remove(currentCase.getCaseID());
+    }
+
+    /**
+     *
+     * @return gets the current userName of this user
+     */
+    protected String getUserName() {
         return userName;
     }
 
-    protected int getWorkerID()
-    {
+    /**
+     *
+     * @return gets the workerID of this user
+     */
+    protected int getWorkerID() {
+
         return workerID;
     }
-    
-    
-    
-    
+
 }
