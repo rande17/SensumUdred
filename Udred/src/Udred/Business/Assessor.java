171054a4d58e6c98889c9461d11cbca4105a5c91
Johnny322 <sipoperle@hotmail.com>
2018-05-01 13:20:00 +0200

    Added Assessor Javadoc + getters and setters

--- Udred/src/Udred/Business/Assessor.java
+++ Udred/src/Udred/Business/Assessor.java
@@ -15,13 +15,36 @@ public class Assessor
     private int assessorTelephone;
     private String function;
 
+    /**
+     * Constructor for Assessor
+     * @param assessorName : name of the assessor, used as information about the meeting
+     * @param assessorTelephone : Phone number of the assessor, used to contact the assessor
+     * @param function : The function of the assessor, specifying what the assessor can do at the meeting
+     */
     protected Assessor(String assessorName, int assessorTelephone, String function)
     {
         this.assessorName = assessorName;
         this.assessorTelephone = assessorTelephone;
         this.function = function;
     }
-    
-    
-    
+
+    public String getAssessorName() {
+        return assessorName;
+    }
+
+    public int getAssessorTelephone() {
+        return assessorTelephone;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setAssessorTelephone(int assessorTelephone) {
+        this.assessorTelephone = assessorTelephone;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
 }
