a0fe60c7b2c979fea6d152fec850c20bd8a403b6
Linea Hoffmann <Linea.hoffmann.christophersen@gmail.com>
2018-05-10 17:06:40 +0200

    Database connection

--- Udred/src/Udred/Main.java
+++ Udred/src/Udred/Main.java
@@ -1,11 +1,10 @@
 package Udred;
 
-import java.io.File;
+import Udred.Data.PostgresHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,10 +13,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LoginWindow.fxml"));
         primaryStage.setTitle("Sensum Udred");
-        primaryStage.getIcons().add(new Image(File.pathSeparator+"Udred"+File.pathSeparator+"EG.png"));
+       // primaryStage.getIcons().add(new Image(File.pathSeparator+"Udred"+File.pathSeparator+"EG.png"));
 //        primaryStage.getIcons().add(new Image("file:EG.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        PostgresHelper DB = new PostgresHelper();
+        DB.connect();
     }
 
 
